827b654909f2577dadaaac54a6c625055202e7d0
Evgenii Sopov <mrseakg@gmail.com>
2019-11-27 12:47:54 +0700

    Prepare

---
+++ src/wsjcpp_hashes.cpp
@@ -0,0 +1,24 @@
+#include "wsjcpp_hashes.h"
+#include <dirent.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <sys/time.h>
+#include <time.h>
+#include <ctime>
+#include <math.h>
+#include <chrono>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <thread>
+#include <cstdint>
+#include <unistd.h>
+#include <streambuf>
+
+// ---------------------------------------------------------------------
+
+std::string WSJCppHashes::test() {
+
+}
---
+++ src/wsjcpp_hashes.h
@@ -0,0 +1,17 @@
+#ifndef WSJCPP_HASHES_H
+#define WSJCPP_HASHES_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include <mutex>
+#include <deque>
+#include <iostream>
+
+class WSJCppHashes {
+    public:
+        static std::string test();
+
+};
+
+#endif // WSJCPP_HASHES_H
